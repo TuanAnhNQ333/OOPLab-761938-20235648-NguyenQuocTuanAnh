c13af6ff7a0da130fb51715c0d17d4ff5e220437
AH_NQ <tuananh030305@gmail.com>
2026-03-31 00:24:34 +0700

    lab02

---
+++ src/main/java/com/example/oop_lab/lab2/Aims/DigitalVideoDisc.java
@@ -0,0 +1,74 @@
+package com.example.oop_lab.lab2.Aims;
+
+public class DigitalVideoDisc {
+    /**
+     * tittle, category, director,
+     * int length
+     * float cost
+     */
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+        public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+            this.title = title;
+            this.category = category;
+            this.director = director;
+            this.length = length;
+            this.cost = cost;
+        }
+
+        public DigitalVideoDisc(String title) {
+            this(title, null, null, 0, 0.0f);
+        }
+
+        public DigitalVideoDisc(String title, String category, float cost) {
+            this(title, category, null, 0, cost);
+        }
+
+        public DigitalVideoDisc(String director, String category, String title, float cost) {
+            this(title, category, director, 0, cost);
+        }
+}
