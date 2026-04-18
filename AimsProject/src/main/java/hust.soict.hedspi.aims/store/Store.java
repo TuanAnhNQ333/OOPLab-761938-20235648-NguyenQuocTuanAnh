c3b2af21fdccf76244319e9004867196692a9830
AH_NQ <tuananh030305@gmail.com>
2026-04-18 23:24:27 +0700

    update store

--- AimsProject/src/main/java/hust.soict.hedspi.aims/store/Store.java
+++ AimsProject/src/main/java/hust.soict.hedspi.aims/store/Store.java
@@ -1,6 +1,7 @@
 package hust.soict.hedspi.aims.store;
 
 import hust.soict.hedspi.aims.media.Media;
+import hust.soict.hedspi.aims.cart.Cart;
 import java.util.ArrayList;
 
 public class Store {
@@ -18,6 +19,43 @@ public class Store {
     public void removeMedia(Media media) {
         if(itemsOrdered.remove(media)) {
             System.out.println("The media has been deleted");
+        } else {
+            System.out.println("The media was not found");
+        }
+    }
+
+    // Print all media in store
+    public void printStore() {
+        System.out.println("************* STORE *************");
+        if (itemsOrdered.isEmpty()) {
+            System.out.println("The store is empty");
+            return;
+        }
+
+        int i = 1;
+        for (Media media : itemsOrdered) {
+            System.out.println(i + ". " + media.toString());
+            i++;
+        }
+        System.out.println("*********************************");
+    }
+
+    // Search by title
+    public Media searchByTitle(String title) {
+        for (Media media : itemsOrdered) {
+            if (media.getTitle().equalsIgnoreCase(title)) {
+                return media;
+            }
+        }
+        return null; // không tìm thấy
+    }
+
+    // Remove by title
+    public void removeByTitle(String title) {
+        Media media = searchByTitle(title);
+        if (media != null) {
+            itemsOrdered.remove(media);
+            System.out.println("The media has been removed");
         } else {
             System.out.println("The media was not found");
         }
